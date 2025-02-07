5b79dbf480e09b328876254c9612dbaee8a06d89
bhumikakarri <karrisarika2004@gmail.com>
2025-02-07 14:15:21 +0530

    new changes

--- src/Components/Signup.jsx
+++ src/Components/Signup.jsx
@@ -32,7 +32,7 @@ function Signup() {
             />
           </div>
           <div className='form-action'>
-            <Link to='/'>
+            <Link to='/reactworks'>
               <button type="submit" className="btn btn-primary w-100">Signup</button>
             </Link>
           </div>
